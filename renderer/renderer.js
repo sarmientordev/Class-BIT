c6fbbbdfd27c84821d6a9a0c08f1c3ce9e8ac086
Rafael Sarmiento <rafasarmiento777@gmail.com>
2026-08-25 01:53:16 -0500

    Use background-removed 8-bit Spider-Man logo, revert header buttons to mint #4DD09E

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -557,7 +557,7 @@ const THEME_DEFAULTS = {
   light: { text: '#33264f', classname: '#33264f', buttons: '#b32a6e', time: '#0aa893', hover: '#d63c8c', lilac: '#6a4fd0', arrow: '#0aa893', titlebar: '#f4f1fa' },
   forest:   { text: '#d4ffe8', classname: '#d4ffe8', buttons: '#00cc6a', time: '#00ff88', hover: '#00ff88', lilac: '#00e5cc', arrow: '#00e5cc', titlebar: '#020c06' },
   synthwave:{ text: '#fff0e8', classname: '#fff0e8', buttons: '#ff3e96', time: '#ff6b35', hover: '#ff6b35', lilac: '#ffaa00', arrow: '#00d4ff', titlebar: '#050510' },
-  spider:   { text: '#0d2137', classname: '#cc0000', buttons: '#32cd32', time: '#cc0000', hover: '#ff4444', lilac: '#1a3a5c', arrow: '#cc0000', titlebar: '#081a30' },
+  spider:   { text: '#0d2137', classname: '#cc0000', buttons: '#cc0000', time: '#cc0000', hover: '#ff4444', lilac: '#1a3a5c', arrow: '#cc0000', titlebar: '#081a30' },
   custom:{ text: '#f4d0ff', classname: '#f4d0ff', buttons: '#c0176c', time: '#e054a0', hover: '#e054a0', lilac: '#9b5de5', arrow: '#00f5d4', titlebar: '#0d0618' },
 };
 
